78ecec249fa2a0c57d18bfcc46f470a91c01851e
bart <wojtowicz.bartlomiej@gmail.com>
2014-12-12 00:43:48 +0100

    bump to 1.5.0

--- core/src/com/softkiwi/libgdx/issues/nosoundiphone/IssueReport.java
+++ core/src/com/softkiwi/libgdx/issues/nosoundiphone/IssueReport.java
@@ -9,28 +9,30 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class IssueReport extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+    SpriteBatch batch;
+    Texture img;
+
     Music music;
-	
-	@Override
-	public void create () {
+
+    @Override
+    public void create() {
         super.create();
 
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+
         music = Gdx.audio.newMusic(new FileHandle("test.m4a"));
         music.play();
-	}
+    }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
 
     @Override
     public void dispose() {
